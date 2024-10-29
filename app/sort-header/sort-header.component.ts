e3e7b2f77ff5658134dd6d945a557ae540b9ac9f
PrTSrc18 <167572365+PrTSrc18@users.noreply.github.com>
2024-10-29 17:29:13 +0000

    Add files via upload

---
+++ app/sort-header/sort-header.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-sort-header',
+  standalone: true,
+  imports: [],
+  templateUrl: './sort-header.component.html',
+  styleUrl: './sort-header.component.css'
+})
+export class SortHeaderComponent {
+
+}
